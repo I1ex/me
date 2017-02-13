83ade630a1dbe784200b3503315a0d27fde1c0ea
I1ex <sashamexonya@yandex.ru>
2017-02-13 20:55:04 +0500

    Create algorithm.php

---
+++ algorithm.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+<?php
+
+$x = rand(1,100);
+//$x = $_GET["x"];
+$var1 = 1;
+$var2 = 1;
+ echo "$x </br>";
+
+while ($var1 <=  $x)
+{
+	if ($var1 > $x )
+	{
+		echo 'Задуманное число Не входит в числовой ряд';
+		
+	}
+	elseif ($var1 === $x)
+	{
+		echo 'Задуманное число входит в числовой ряд';
+	} 
+$var1++;
+ }
